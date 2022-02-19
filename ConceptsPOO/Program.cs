b893291a78721e30555cbd0c967f67f6913730a1
David <david_horeb@hotmail.com>
2022-02-19 12:31:38 -0300

    Add Class Date

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 28));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
